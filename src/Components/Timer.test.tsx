fba9b152bf80f761a65afb98db7bb79fdd4ef5e3
Mudassir Khan <38735197+Muddi00seven@users.noreply.github.com>
2020-09-01 17:44:47 +0500

    TImer Test Update

--- src/Components/Timer.test.tsx
+++ src/Components/Timer.test.tsx
@@ -1,4 +1,17 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import TimerClock from './TimerClock'
+
+Enzyme.configure({adapter: new Adapter})
+let wrapper = shallow(<TimerClock/>)
+
+describe('TimerClock', () => {
+    it('it should be render Timer Clock' , () => {
+        shallow(<TimerClock/>)
+    });
+    it('should be render DIV', () => {
+        expect(wrapper.find('div').length).toBeGreaterThan(2)
+    })
+
+})
