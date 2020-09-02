7e087de6e14e617e6060aa9a9f6e848c28b2ad89
Mudassir Khan <38735197+Muddi00seven@users.noreply.github.com>
2020-09-02 13:45:02 +0500

    pwa updated

---
+++ public/sw.js
@@ -0,0 +1,35 @@
+var CACHE_NAME = "web";
+var urlsToCache = [
+  "/static/js/bundle.js",
+  "/static/js/main.chunk.js",
+  "/static/js/0.chunk.js",
+  "index.html",
+  "/",
+];
+
+self.addEventListener("install", function (event) {
+  // Perform install steps
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then(function (cache) {
+        console.log("Opened cache");
+        return cache.addAll(urlsToCache);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
